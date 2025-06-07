51964784868b3a5265a91174935e022f9583c041
Subhradeep1708 <subhradeep1708@gmail.com>
2025-06-07 12:42:03 +0530

    Refactor global styles and add DashboardIcon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import DashboardIcon from "./components/DashboardIcon";
 
 export default function Home() {
   return (
@@ -51,6 +52,11 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+
+      <DashboardIcon />
+
+
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
